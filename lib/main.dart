f8ab963e6ea0c81407db30c43a748e6641f9c344
Gabriel <135984152+gsgabs@users.noreply.github.com>
2025-06-03 20:10:03 -0300

    Update: main.dart
    added dices and other customizations

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,20 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.lightBlue[200],
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text(
+                'DADOS',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                ),
+            ),
+          ),
           backgroundColor: Colors.red,
+          shadowColor: Colors.pink[700],
+          elevation: 10,
         ),
         body: DicePage(),
       ),
@@ -20,7 +30,30 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 10.0,
+              ),
+              child: Image.asset('assets/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 10.0,
+                right: 20.0,
+              ),
+              child: Image.asset('assets/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
